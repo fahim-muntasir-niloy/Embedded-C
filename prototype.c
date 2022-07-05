672e9b8ca97980665c6635cafe602d5f3029a7cf
Fahim Muntasir <1601107@student.ruet.ac.bd>
2022-07-05 13:15:52 +0600

    added another function with if else

--- prototype.c
+++ prototype.c
@@ -1,15 +1,26 @@
 #include<stdio.h>
 
 //prototyping is way of using function after the main function.
+// this is how you protoype
 void randFunc();
+int maxNum(int a, int b);
 
 int main(int argc, const char** argv) {
-
     randFunc();
+    maxNum(2213,34);
     return 0;
 }
 
-//this is a random function
+//this are some functions
 void randFunc(){
-    printf("This is a random print");
+    printf("This is a random print\n");
+}
+
+int maxNum(int a, int b){
+    if (a>b){
+        printf("%d",a);
+    }
+    else{
+        printf("%d",b);
+    }
 }
