cf2e18147e3993186794d339402de050bf6a7ba2
Fahim Muntasir <1601107@student.ruet.ac.bd>
2022-07-05 12:38:17 +0600

    prototype in functions

---
+++ prototype.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+//prototyping is way of using function after the main function.
+
+int main(int argc, const char** argv) {
+
+    randFunc();
+    return 0;
+}
+
+//this is a random function
+void randFunc(){
+    printf("This is a random print");
+}
